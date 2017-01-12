25227086dd9e18c8ebcc8b683b7cb6ad9276e27a
gcbest <godfrey.c.best@gmail.com>
2017-01-11 23:57:25 -0500

    create task

--- server.js
+++ server.js
@@ -10,24 +10,33 @@ app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
-var account_sid = "ACe7a837186774c62a5c084d883471ff79"
-var auth_token  = "0073a6844335c258559ee77e76dedc8e"
-var workspace_sid = "WQdf1a595e3cd1c5af8cb82d3d13699c2e"
-var workflow_sid = "WWf8ca2679597d21271bb9ef9f021780ab"
+var accountSid = "ACe7a837186774c62a5c084d883471ff79"
+var authToken  = "0073a6844335c258559ee77e76dedc8e"
+var workspaceSid = "WS9015ca51b9ab6115a86bed25f66e0af6"
+var workflowSid = "WW7c2e1725801f4c07058d862f5638983a"
 
-var client = twilio.TwilioTaskRouterClient(account_sid, auth_token)
+// var client = new twilio.TaskRouterClient(accountSid, authToken);
+// var client = require('twilio')(accountSid, authToken);
 
 app.post("/assignment_callback", function(req, res) {
 	res.send({});
 });
 
 app.post("/create_task", function(req, res) {
-	var task = client.tasks(workspace_sid).create(
-        workflow_sid=workflow_sid,
-        attributes='{"selected_week": 2}'
-    );
+	// var task = client.tasks(workspaceSid).create(
+ //        workflowSid=workflowSid,
+ //        attributes='{"selected_week": 2}'
+ //    );
 
-    console.log(task.attributes);
+ //    console.log(task.attributes);
+
+ var client = new twilio.TaskRouterClient(accountSid, authToken, workspaceSid);
+
+client.workspace.tasks.list(function(err, data) {
+    data.tasks.forEach(function(task) {
+        console.log(task.attributes);
+    })
+});
     res.send({});
 });
 
